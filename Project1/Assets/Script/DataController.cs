7b96281436942159b8121dc9cfebbdb98c3b8ea8
roller0516 <68807226+roller0516@users.noreply.github.com>
2020-08-24 18:20:50 +0900

    [프로그래밍]

--- Project1/Assets/Script/DataController.cs
+++ Project1/Assets/Script/DataController.cs
@@ -6,20 +6,25 @@ using UnityEngine.UI;
 public class DataController : MonoBehaviour
 {
     public Text Text_;
+    private int Count = 4 ;
     private int Food;
-    
-    public TimeScroll[] time_scroll;
+    [SerializeField]
+    private TimeScroll[] time_scroll;
     void Start()
-    {   for (int i = 0; i > time_scroll.Length; ++i) 
+    {
+        time_scroll = new TimeScroll[Count];
+        Count = 1;
+        for (int i = 0; i<time_scroll.Length;++i) 
         {
-            time_scroll[i] = GameObject.FindWithTag("TimeScroll").GetComponent<TimeScroll>();
+            time_scroll[i] = GameObject.Find("TimeSlider"+Count).GetComponent<TimeScroll>();
+            Count++;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (time_scroll[0].isDone == true) 
+        if (time_scroll[0].isDone == true)
         {
             time_scroll[0].isDone = false;
             Food += 100;
@@ -34,7 +39,7 @@ public class DataController : MonoBehaviour
             time_scroll[2].isDone = false;
             Food += 10000;
         }
-        if (time_scroll[3].isDone == true)
+        if (time_scroll[3] != null && time_scroll[3].isDone == true)
         {
             time_scroll[3].isDone = false;
             Food += 100000;
